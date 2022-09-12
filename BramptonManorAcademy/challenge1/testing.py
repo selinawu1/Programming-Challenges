a7b8b722510a35041dcd584702c7d0ffb67346c8
selinawu1 <112864607+selinawu1@users.noreply.github.com>
2022-09-12 11:32:32 +0100

    Update testing.py

--- BramptonManorAcademy/challenge1/testing.py
+++ BramptonManorAcademy/challenge1/testing.py
@@ -1,1 +1,13 @@
+import unittest
 
+from rod_conversions import *
+
+
+class MyFirstTests(unittest.TestCase):
+
+    def test_rodsIntoMeters(self):
+        self.assertEqual(rodsIntoMeters(1), 5.0292)
+
+
+if __name__ == '__main__':
+    unittest.main()
